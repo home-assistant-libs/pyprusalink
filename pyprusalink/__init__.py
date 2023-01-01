cd4c5e02494cdd239df3fc45f117f92ce3389d33
John Rodewald <jnrodewald99@gmail.com>
2023-01-01 17:46:08 +0100

    Replace strings with consts and update class shape

--- pyprusalink/__init__.py
+++ pyprusalink/__init__.py
@@ -4,9 +4,16 @@ from __future__ import annotations
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 from typing import TypedDict
-from httpx import Response, AsyncClient
+from httpx import Response, AsyncClient, BasicAuth
 
-import httpx
+API_KEY = "apiKey"
+API_KEY_AUTH = "ApiKeyAuth"
+AUTH = "auth"
+AUTH_TYPE = "authType"
+DIGEST_AUTH = "DigestAuth"
+HOST = "host"
+PASSWORD = "password"
+USER = "user"
 
 class PrusaLinkError(Exception):
     """Base class for PrusaLink errors."""
@@ -61,14 +68,14 @@ class FilesInfo(TypedDict):
 class UserAuth(TypedDict):
     """Authentication via `user` + `password` digest"""
 
-    type: "UserAuth"
+    type: DIGEST_AUTH
     user: str
     password: str
 
 class ApiKeyAuth(TypedDict):
     """Authentication via api key"""
 
-    type: "ApiKeyAuth"
+    type: API_KEY_AUTH
     apiKey: str
 
 class LinkConfiguration(TypedDict):
@@ -76,7 +83,6 @@ class LinkConfiguration(TypedDict):
 
     host: str
     auth: UserAuth | ApiKeyAuth
-
 
 class PrusaLink:
     """Wrapper for the Prusalink API.
@@ -88,16 +94,11 @@ class PrusaLink:
     def __init__(self, client: AsyncClient, config: LinkConfiguration) -> None:
         """Initialize the PrusaLink class."""
 
-        auth = config["auth"]
+        print("pyprusalink config:", config)
 
-        if auth["type"] == "UserAuth":
-            self.password = auth["password"]
-            self.user = auth["user"]
-        if auth["type"] == "ApiKeyAuth":
-            self.apiKey = auth["apiKey"]
-        self.host = config["host"]
-        self.authType = auth["type"]
+        self.host = config[HOST]
         self.client = client
+        self.auth = config[AUTH]
 
     async def cancel_job(self) -> None:
         """Cancel the current job."""
@@ -121,37 +122,37 @@ class PrusaLink:
     async def get_version(self) -> VersionInfo:
         """Get the version."""
         async with self.request("GET", "api/version") as response:
-            return response.json()
+            return await response.json()
 
     async def get_printer(self) -> PrinterInfo:
         """Get the printer."""
         async with self.request("GET", "api/printer") as response:
-            return response.json()
+            return await response.json()
 
     async def get_job(self) -> JobInfo:
         """Get current job."""
         async with self.request("GET", "api/job") as response:
-            return response.json()
+            return await response.json()
 
     async def get_file(self, path: str) -> FileInfo:
         """Get specific file info."""
         async with self.request("GET", f"api/files{path}") as response:
-            return response.json()
+            return await response.json()
 
     async def get_files(self) -> FilesInfo:
         """Get all files."""
         async with self.request("GET", "api/files?recursive=true") as response:
-            return response.json()
+            return await response.json()
 
     async def get_small_thumbnail(self, path: str) -> bytes:
         """Get a small thumbnail."""
         async with self.request("GET", f"thumb/s{path}") as response:
-            return response.read()
+            return await response.read()
 
     async def get_large_thumbnail(self, path: str) -> bytes:
         """Get a large thumbnail."""
         async with self.request("GET", f"thumb/l{path}") as response:
-            return response.read()
+            return await response.read()
 
     @asynccontextmanager
     async def request(
@@ -161,12 +162,12 @@ class PrusaLink:
         url = f"{self.host}/{path}"
 
         auth = None
-        if self.authType == "UserAuth":
-            auth = httpx.BasicAuth(self.user, self.password)
+        if self.auth[AUTH_TYPE] == DIGEST_AUTH:
+            auth = BasicAuth(self.auth[USER], self.auth[PASSWORD])
 
         headers = {}
-        if self.authType == "ApiKeyAuth":
-            headers = {"X-Api-Key": self.apiKey}
+        if self.auth[AUTH_TYPE] == API_KEY_AUTH:
+            headers = {"X-Api-Key": self.auth[API_KEY]}
 
         client = self.client
         async with client:
@@ -174,9 +175,11 @@ class PrusaLink:
                 method, url, json=json, auth=auth, headers=headers, 
             )
 
-            if response.status_code == 401:
+            print('pyprusalink response:')
+            print(response)
+            if response.status == 401:
                 raise InvalidAuth()
-            if response.status_code == 409:
+            if response.status == 409:
                 raise Conflict()
             response.raise_for_status()
 

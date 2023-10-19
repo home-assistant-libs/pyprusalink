166751dca02f954223afb98df8d261e0cfd4b275
Niklas Wagner <Skaro@Skaronator.com>
2023-10-19 09:20:15 +0200

    Switch to v1 API endpoints and use DigestAuth

--- pyprusalink/__init__.py
+++ pyprusalink/__init__.py
@@ -1,146 +1,62 @@
 """Prusalink API."""
 from __future__ import annotations
+from aiohttp import ClientSession
 
-from collections.abc import AsyncGenerator
-from contextlib import asynccontextmanager
-from typing import TypedDict
-
-from aiohttp import ClientResponse, ClientSession
-
-
-class PrusaLinkError(Exception):
-    """Base class for PrusaLink errors."""
-
-
-class InvalidAuth(PrusaLinkError):
-    """Error to indicate there is invalid auth."""
-
-
-class Conflict(PrusaLinkError):
-    """Error to indicate the command hit a conflict."""
-
-
-class VersionInfo(TypedDict):
-    """Version data."""
-
-    api: str
-    server: str
-    text: str
-    hostname: str
-
-
-class PrinterInfo(TypedDict):
-    """Printer data."""
-
-    telemetry: dict
-    temperature: dict
-    state: dict
-
-
-class JobInfo(TypedDict):
-    """Job data."""
-
-    state: str
-    job: dict | None
-
-
-class FileInfo(TypedDict):
-    """File data."""
-
-    name: str
-    origin: str
-    size: int
-    refs: dict
-
-
-class FilesInfo(TypedDict):
-    """Files data."""
-
-    files: list[FileInfo]
+from pyprusalink.types import VersionInfo, PrinterInfo, PrinterStatus, JobInfo
+from pyprusalink.client import ApiClient
 
 
 class PrusaLink:
     """Wrapper for the Prusalink API.
 
     Data format can be found here:
-    https://github.com/prusa3d/Prusa-Firmware-Buddy/blob/master/lib/WUI/link_content/basic_gets.cpp
+    https://github.com/prusa3d/Prusa-Link-Web/blob/master/spec/openapi.yaml
     """
 
-    def __init__(self, session: ClientSession, host: str, api_key: str) -> None:
+    def __init__(self, session: ClientSession, host: str, username: str, password: str) -> None:
         """Initialize the PrusaLink class."""
-        self._session = session
-        self.host = host
-        self._api_key = api_key
+        self.client = ApiClient(session=session, host=host, username=username, password=password)
 
-    async def cancel_job(self) -> None:
+    async def cancel_job(self, jobId: int) -> None:
         """Cancel the current job."""
-        async with self.request("POST", "api/job", {"command": "cancel"}):
+        async with self.client.request("DELETE", f"/api/v1/job/{jobId}"):
             pass
 
-    async def resume_job(self) -> None:
+    async def pause_job(self, jobId: int) -> None:
+        """Pause a job."""
+        async with self.client.request("PUT", f"/api/v1/job/{jobId}/pause"):
+            pass
+
+    async def resume_job(self, jobId: int) -> None:
         """Resume a paused job."""
-        async with self.request(
-            "POST", "api/job", {"command": "pause", "action": "resume"}
-        ):
-            pass
-
-    async def pause_job(self) -> None:
-        """Pause the current job."""
-        async with self.request(
-            "POST", "api/job", {"command": "pause", "action": "pause"}
-        ):
+        async with self.client.request("PUT", f"/api/v1/job/{jobId}/resume"):
             pass
 
     async def get_version(self) -> VersionInfo:
         """Get the version."""
-        async with self.request("GET", "api/version") as response:
+        async with self.client.request("GET", "/api/version") as response:
             return await response.json()
 
-    async def get_printer(self) -> PrinterInfo:
+    async def get_info(self) -> PrinterInfo:
         """Get the printer."""
-        async with self.request("GET", "api/printer") as response:
+        async with self.client.request("GET", "/api/v1/info") as response:
+            return await response.json()
+
+    async def get_status(self) -> PrinterStatus:
+        """Get the printer."""
+        async with self.client.request("GET", "/api/v1/status") as response:
             return await response.json()
 
     async def get_job(self) -> JobInfo:
         """Get current job."""
-        async with self.request("GET", "api/job") as response:
+        async with self.client.request("GET", "/api/v1/job") as response:
+            # when there is no job running we'll an empty document that will fail to parse
+            if response.status == 204:
+                return {}
             return await response.json()
 
-    async def get_file(self, path: str) -> FileInfo:
-        """Get specific file info."""
-        async with self.request("GET", f"api/files{path}") as response:
-            return await response.json()
-
-    async def get_files(self) -> FilesInfo:
-        """Get all files."""
-        async with self.request("GET", "api/files?recursive=true") as response:
-            return await response.json()
-
-    async def get_small_thumbnail(self, path: str) -> bytes:
-        """Get a small thumbnail."""
-        async with self.request("GET", f"thumb/s{path}") as response:
+    # Prusa Link Web UI still uses the old endpoints and it seems that the new v1 endpoint doesn't support this yet
+    async def get_file(self, path: str) -> bytes:
+        """Get a files such as Thumbnails or Icons. Path comes from the current job['file']['refs']['thumbnail']"""
+        async with self.client.request("GET", path) as response:
             return await response.read()
-
-    async def get_large_thumbnail(self, path: str) -> bytes:
-        """Get a large thumbnail."""
-        async with self.request("GET", f"thumb/l{path}") as response:
-            return await response.read()
-
-    @asynccontextmanager
-    async def request(
-        self, method: str, path: str, json: dict | None = None
-    ) -> AsyncGenerator[ClientResponse, None]:
-        """Make a request to the PrusaLink API."""
-        url = f"{self.host}/{path}"
-        headers = {"X-Api-Key": self._api_key}
-
-        async with self._session.request(
-            method, url, headers=headers, json=json
-        ) as response:
-            if response.status == 401:
-                raise InvalidAuth()
-            if response.status == 409:
-                raise Conflict()
-
-            response.raise_for_status()
-            yield response
---
+++ pyprusalink/types.py
@@ -0,0 +1,115 @@
+from typing import TypedDict, Optional
+from enum import Enum
+
+
+class PrusaLinkError(Exception):
+    """Base class for PrusaLink errors."""
+
+
+class InvalidAuth(PrusaLinkError):
+    """Error to indicate there is invalid auth."""
+
+
+class Conflict(PrusaLinkError):
+    """Error to indicate the command hit a conflict."""
+
+
+class Capabilities(TypedDict):
+    """API Capabilities"""
+    upload_by_put: Optional[bool]
+
+
+class VersionInfo(TypedDict):
+    """Version data."""
+    api: str
+    version: str
+    printer: str
+    text: str
+    firmware: str
+    sdk: Optional[str]
+    capabilities: Optional[Capabilities]
+
+
+class PrinterInfo(TypedDict):
+    """Printer informations."""
+    mmu: Optional[bool]
+    name: Optional[str]
+    location: Optional[str]
+    farm_mode: Optional[bool]
+    nozzle_diameter: Optional[float]
+    min_extrusion_temp: Optional[int]
+    serial: Optional[str]
+    sd_ready: Optional[bool]
+    active_camera: Optional[bool]
+    hostname: Optional[str]
+    port: Optional[str]
+    network_error_chime: Optional[bool]
+
+
+class StatusInfo(TypedDict):
+    """Status of the printer."""
+    ok: Optional[bool]
+    message: Optional[str]
+
+class PrinterState(Enum):
+    IDLE = "IDLE"
+    BUSY = "BUSY"
+    PRINTING = "PRINTING"
+    PAUSED = "PAUSED"
+    FINISHED = "FINISHED"
+    STOPPED = "STOPPED"
+    ERROR = "ERROR"
+    ATTENTION = "ATTENTION"
+    READY = "READY"
+
+class PrinterStatusInfo(TypedDict):
+    """Printer information."""
+    state: PrinterState
+    temp_nozzle: Optional[float]
+    target_nozzle: Optional[float]
+    temp_bed: Optional[float]
+    target_bed: Optional[float]
+    axis_x: Optional[float]
+    axis_y: Optional[float]
+    axis_z: Optional[float]
+    flow: Optional[int]
+    speed: Optional[int]
+    fan_hotend: Optional[int]
+    fan_print: Optional[int]
+    status_printer: Optional[StatusInfo]
+    status_connect: Optional[StatusInfo]
+
+class PrinterStatus(TypedDict):
+    """Printer status."""
+    printer: PrinterStatusInfo
+
+
+class PrintFileRefs(TypedDict):
+    """Additional Files for the current Job"""
+    download: Optional[str]
+    icon: Optional[str]
+    thumbnail: Optional[str]
+
+
+class JobFilePrint(TypedDict):
+    """Currently printed file informations."""
+    name: str
+    display_name: Optional[str]
+    path: str
+    display_path: Optional[str]
+    size: Optional[int]
+    m_timestamp: int
+    meta: Optional[dict]
+    refs: Optional[PrintFileRefs]
+
+
+class JobInfo(TypedDict):
+    """Job information."""
+    id: int
+    state: str
+    progress: int
+    time_remaining: Optional[int]
+    time_printing: int
+    inaccurate_estimates: Optional[bool]
+    serial_print: Optional[bool]
+    file: Optional[JobFilePrint]
